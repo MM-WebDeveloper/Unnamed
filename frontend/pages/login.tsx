a088baa1f196b35d5e08b5378d8e4f94c739f9a0
milan-dev <milan.varnamo@gmail.com>
2021-11-14 14:13:22 +0100

    initial frontend commit

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface loginProps {}
+
+const login: React.FC<loginProps> = ({}) => {
+  return <div>Login Page</div>;
+};
+export default login;
---
+++ frontend/pages/register.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface registerProps {}
+
+const register: React.FC<registerProps> = ({}) => {
+  return <div>Register Page</div>;
+};
+export default register;
