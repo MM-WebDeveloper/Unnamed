ec8915f735d464a9719ba6d1728991b61388156e
milan-dev <milan.varnamo@gmail.com>
2021-11-15 20:52:45 +0100

    added a reveal and hide  password toggle button to the register password input

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 import Footer from '../components/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/Register.module.css';
 
 interface registerProps {}
 
 const register: React.FC<registerProps> = ({}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [revealPassword, setRevealPassword] = useState(true);
 
-  const onSubmit = () => {
-    console.log('happened');
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(email, password);
   };
+
   return (
     <div>
-      <form className={'form'} onSubmit={onSubmit}>
+      <form className={'form'} onSubmit={onSubmitHandler}>
         <div>
           <label htmlFor="email" />
           <input
@@ -29,11 +36,18 @@ const register: React.FC<registerProps> = ({}) => {
           <input
             className={'form__input'}
             value={password}
-            type="password"
+            type={revealPassword ? 'password' : 'text'}
             placeholder="password"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
+          />
+          <FontAwesomeIcon
+            className={styles.password_btn}
+            icon={faEye}
+            size="2x"
+            onClick={() => setRevealPassword(!revealPassword)}
+            title="Show password"
           />
         </div>
         <button className={'form__btn'} type="submit">
