ee652d93de6cc9851a9021275f6427c44aa1c0f4
milan-dev <milan.varnamo@gmail.com>
2021-11-16 20:28:28 +0100

    added the mongodb atlas connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,25 @@ import { GraphQLServer } from 'graphql-yoga';
 import 'reflect-metadata';
 import { buildTypeDefsAndResolvers } from 'type-graphql';
 import { UserResolver } from './resolvers/UserResolver';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const run = async () => {
+  const PORT = process.env.PORT || 4000;
+
+  await mongoose
+    .connect(process.env.MONGO_DB_URI!)
+    .then(() => console.log('Connected to MongoDB Atlas...'))
+    .catch((err) => {
+      console.log(err);
+    });
+
   const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
     resolvers: [UserResolver],
   });
 
   const options = {
-    port: 4000,
+    port: PORT,
     endpoint: '/graphql',
     playground: '/playground',
   };
@@ -17,7 +28,7 @@ const run = async () => {
   const server = new GraphQLServer({ typeDefs, resolvers });
 
   server.start(options, () =>
-    console.log(`Server started: http://localhost:4000/playground`)
+    console.log(`Server started: http://localhost:${PORT}/playground`)
   );
 };
 
