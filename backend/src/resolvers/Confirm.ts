fac98728471af6d39d54d252640db15f34004817
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-01-02 16:25:36 +0100

    removed unnecessary console logs

--- backend/src/resolvers/Confirm.ts
+++ backend/src/resolvers/Confirm.ts
@@ -5,35 +5,35 @@ import 'dotenv/config';
 
 @ObjectType()
 class ConfirmResponse {
-  @Field(() => Boolean, { nullable: true })
-  success?: boolean;
-  @Field(() => String, { nullable: true })
-  error?: string;
+	@Field(() => Boolean, { nullable: true })
+	success?: boolean;
+	@Field(() => String, { nullable: true })
+	error?: string;
 
-  constructor(success?: boolean, error?: string) {
-    this.success = success;
-    this.error = error;
-  }
+	constructor(success?: boolean, error?: string) {
+		this.success = success;
+		this.error = error;
+	}
 }
 
 @Resolver()
 export class ConfirmResolver {
-  @Mutation(() => ConfirmResponse)
-  async confirm(@Arg('token') token: string): Promise<ConfirmResponse> {
-    try {
-      const decodedToken = jwt.verify(
-        token,
-        process.env.JWT_EMAIL_SECRET!
-      ) as JwtPayload;
+	@Mutation(() => ConfirmResponse)
+	async confirm(@Arg('token') token: string): Promise<ConfirmResponse> {
+		try {
+			const decodedToken = jwt.verify(
+				token,
+				process.env.JWT_EMAIL_SECRET!
+			) as JwtPayload;
 
-      await UserModel.findOne({ id: decodedToken.id }).update({
-        confirmed: true,
-      });
+			await UserModel.findOne({ id: decodedToken.id }).update({
+				confirmed: true,
+			});
 
-      return new ConfirmResponse(true, undefined);
-    } catch (error) {
-      console.log(error);
-      return new ConfirmResponse(false, '500 || Internal Server Error');
-    }
-  }
+			return new ConfirmResponse(true, undefined);
+		} catch (error) {
+			console.log(error);
+			return new ConfirmResponse(false, '500 || Internal Server Error');
+		}
+	}
 }
--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -2,27 +2,26 @@ import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 export const sendEmail = async (email: string, url: string) => {
-  console.log(process.env.EMAIL_PWD);
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.outlook.com',
-    auth: {
-      user: 'milan.varnamo@outlook.com',
-      pass: process.env.EMAIL_PWD,
-    },
-  });
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.outlook.com',
+		auth: {
+			user: 'milan.varnamo@outlook.com',
+			pass: process.env.EMAIL_PWD,
+		},
+	});
 
-  const options = {
-    from: 'milan.varnamo@outlook.com',
-    to: email,
-    subject: 'PhotoDiary || Confirm your account',
-    text: 'Confirm your account by clicking the link below.',
-    html: `<a href="${url}">${url}</a>`,
-  };
+	const options = {
+		from: 'milan.varnamo@outlook.com',
+		to: email,
+		subject: 'PhotoDiary || Confirm your account',
+		text: 'Confirm your account by clicking the link below.',
+		html: `<a href="${url}">${url}</a>`,
+	};
 
-  transporter.sendMail(options, (err, info) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(info);
-  });
+	transporter.sendMail(options, (err, info) => {
+		if (err) {
+			console.log(err);
+		}
+		console.log(info);
+	});
 };
